3a09591291efa821f59ae6da971a54b2e16a48db
Benjamin Pannell <admin@sierrasoftworks.com>
2014-01-01 23:56:36 +0200

    Added further tests

--- test/basic.js
+++ test/basic.js
@@ -15,3 +15,20 @@ it('should correctly allow instantiation of a forge', function() {
 	should.exist(forge2);
 	forge2.should.be.instanceOf(SiteForge);
 });
+
+it('should correctly load compilers', function() {
+	['jade', 'less'].forEach(function(compiler) {
+		SiteForge.compilers.should.have.ownProperty(compiler);
+	});
+});
+
+it('should correctly prepare default options', function() {
+	var forge = new SiteForge();
+	forge.options.should.eql({
+		views: process.cwd() + '/views',
+		engine: 'jade',
+		static: process.cwd() + '/static',
+		output: process.cwd() + '/site',
+		compilers: SiteForge.compilers
+	});
+});
